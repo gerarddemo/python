391f4e373a08d0b61a520e12ac55a1da8ddb8905
Gerard <al.gerarddomenech@iesramonllull.net>
2023-10-31 12:59:25 +0100

    1r commit

---
+++ prueba.py
@@ -0,0 +1,21 @@
+print("hola mundo")
+#soy un comentario
+"""
+texto q no s va a interpretar
+"""
+#variables
+texto = "Repaso de Python con Gerard"
+nombre = "Gerard"
+year = 2023
+print(year)
+#entrada
+sitioweb = input("¿Cual es tu pág web?: ")
+print(sitioweb)
+#condiciones
+altura = int(input("Cual es tu altura?: "))
+
+if altura >= 180: print("Eres alto!")
+else: print("Puto enano!")
+"""
+#funciones
+def mostraraltura
